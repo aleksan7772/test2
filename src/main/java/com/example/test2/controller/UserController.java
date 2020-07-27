238078fe2801edfd7a3af4a466e2693e46f98771
Romchansky <romkapetrenko8@gmail.com>
2020-07-27 21:05:17 +0300

    27.07

--- src/main/java/com/example/test2/controller/UserController.java
+++ src/main/java/com/example/test2/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.example.test2.controller;
 
+import com.example.test2.dto.UserDto;
 import com.example.test2.model.User;
 import com.example.test2.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,13 +15,44 @@ import java.util.List;
  * Created by Brainacad4 on 22.07.2020.
  */
 @Controller
+@RequestMapping(value = "users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping(value = "users")
+    @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDto user) {
+        return new ResponseEntity<User>(userService.saveNewUser(user), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody UserDto user,
+                                           @PathVariable(value = "id") Integer id) {
+        return new ResponseEntity<>(userService.updateUser(id,user), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable(value = "id") Integer id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<Object>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable(value = "id") Integer id) {
+        User user = userService.getUserById(id);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
+
+
+
--- src/main/java/com/example/test2/service/UserService.java
+++ src/main/java/com/example/test2/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.test2.service;
 
+import com.example.test2.dto.UserDto;
 import com.example.test2.model.User;
 import com.example.test2.repository.UserRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -20,13 +23,35 @@ public class UserService {
 
     public User getUserEmeil(String emeil) {
         return userRepository.findByEmail(emeil);
+
     }
 
-    public User saveNewUser(String emeil, String password, String adress) {
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveNewUser(UserDto user) {
         User newUser = new User();
-        newUser.setEmail(emeil);
-        newUser.setPassword(password);
-        newUser.setAdress(adress);
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+        newUser.setAdress(user.getAdress());
+        newUser.setCreationDate(LocalDate.now());
         return userRepository.save(newUser);
+    }
+
+    public User updateUser(Integer id, UserDto updateUser) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("не найден пользователь в БД"));
+        user.setEmail(updateUser.getEmail());
+        user.setPassword(updateUser.getPassword());
+        user.setAdress(updateUser.getAdress());
+        user.setCreationDate(LocalDate.now());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+
     }
 }
